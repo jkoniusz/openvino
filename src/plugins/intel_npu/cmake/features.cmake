e51e4d8125288fb749de8174061b2f27b7f70543
jkoniusz <jakub.koniuszewski@intel.com>
2026-09-24 14:38:51 +0200

    cvs

--- src/plugins/intel_npu/cmake/features.cmake
+++ src/plugins/intel_npu/cmake/features.cmake
@@ -5,7 +5,7 @@
 ov_option(ENABLE_NPU_PLUGIN_ENGINE "Enable compilation of NPU plugin engine" ON)
 
 if(NOT ENABLE_NPU_PLUGIN_ENGINE AND ENABLE_TESTS)
-    message(FATAL_ERROR "Tests depends on npu plugin engine!")
+    message(FATAL_ERROR "Tests depends on npu plugin engine!!")
 endif()
 
 ov_dependent_option(ENABLE_INTEL_NPU_PROTOPIPE "Enable Intel NPU Protopipe tool" ON "ENABLE_INTEL_NPU_INTERNAL" OFF)
